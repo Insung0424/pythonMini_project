e6e7e1dc7b4d9233f6e962aa0a8eb92e6887115d
INS0424 <zhrudrhks@gmail.com>
2022-10-17 18:24:26 +0900

    종목코드.csv 자동 최신화추가

--- pythonProject6/StockFinder.py
+++ pythonProject6/StockFinder.py
@@ -1,5 +1,5 @@
 import sys
-
+import mpl_finance
 import pandas
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -18,17 +18,17 @@ from tkinter import messagebox as msg
 import datetime
 from stockReader import *
 import matplotlib
+from PyQt5.QtCore import QDate
+import matplotlib.ticker as ticker
 
 # 그래프 폰트 크기 조절
 matplotlib.rc('xtick', labelsize=7)
 matplotlib.rc('ytick', labelsize=7)
+matplotlib.rc('font', family='NanumGothic')
 # UI 불러오기
 form_class = uic.loadUiType("stockFinder.ui")[0]
-# 검색 엔진
-engine = 'yahoo'
-# 검색 기간
-start = datetime.datetime(2019, 1, 1)
-end = datetime.datetime.now()
+# # 검색 엔진
+# engine = 'yahoo'
 
 
 class MyWindow(QMainWindow, form_class):
@@ -41,8 +41,13 @@ class MyWindow(QMainWindow, form_class):
         self.setupUi(self)
 
         self.SDR = stockReader()
-        self.get_result = None
 
+        #  코스피 코스닥 종목 최신화
+        self.SDR.write_csv()
+
+        self.dateEdit_2.setDate(QDate.currentDate())  # 검색종료일을 당일로 설정
+
+        self.get_result = None
         self.fig1 = None
         self.top_axes = None
         self.bottom_axes = None
@@ -73,6 +78,20 @@ class MyWindow(QMainWindow, form_class):
         btn6 = self.pushButton_6
         btn6.clicked.connect(self.btn6_clicked)
 
+        btn7 = self.pushButton_7
+        btn7.clicked.connect(self.btn7_clicked)
+
+        btn8 = self.pushButton_8
+        btn8.clicked.connect(self.btn8_clicked)
+        self.pushButton_8.setEnabled(False)
+
+        btn9 = self.pushButton_9
+        btn9.clicked.connect(self.btn9_clicked)
+        self.pushButton_9.setEnabled(False)
+
+        btn10 = self.pushButton_10
+        btn10.clicked.connect(self.btn10_clicked)
+
     def create_canvas(self):
         self.fig1 = plt.figure(figsize=(12, 8))
         self.top_axes = plt.subplot2grid((4, 4), (0, 0), rowspan=3, colspan=4)
@@ -81,8 +100,11 @@ class MyWindow(QMainWindow, form_class):
 
         box1 = self.verticalLayout  # 상하 LayOut 2개를 생성하려다 더 좋은 방법 발견해서 1개로 진행
         box1.addWidget(self.canvas1)  # 그림판 위치 잡기
+        self.pushButton_8.setEnabled(False)
+        self.pushButton_9.setEnabled(False)
 
     def delete_canvas(self):
+        # 역순으로 제거 및 None 타입 변환
         box1 = self.verticalLayout
         box1.removeWidget(self.canvas1)
 
@@ -90,6 +112,16 @@ class MyWindow(QMainWindow, form_class):
         self.top_axes = None
         self.bottom_axes = None
         self.canvas1 = None
+
+    # 버튼2~6까지 반복되는 부분이 많아서 만들어 보았으나 실행은 되지만 에러가 발생함
+    # 함수를 중단할 수 있는 방법이 필요함
+    # def is_result(self):
+    #     result = self.get_result
+    #     if result is None:
+    #         result = msg.showwarning('안내', '종목조회를 먼저해주세요')
+    #         return result
+    # 
+    #     return result
 
     def btn1_clicked(self):
         if self.fig1 is None:
@@ -104,25 +136,33 @@ class MyWindow(QMainWindow, form_class):
             return
 
         code = self.SDR.get_stock_code(id)
-        if self.SDR.get_stock_code(id) is None:
+        if code is None:
             msg.showwarning('안내', '주식의 종목이름을 정확히 입력해주세요')
             return
 
-        code = code + '.ks'
-        print(code)
-        result = self.SDR.get_stock(code, engine, start, end)
-        self.get_result = result
+        start = self.dateEdit.date().toString("yyyy-MM-dd")
+        end = self.dateEdit_2.date().toString("yyyy-MM-dd")
 
-        self.top_axes.plot(result.index, result['Adj Close'], label='Adj Close')
+        result = self.SDR.get_stock(code, start, end)
+
+        self.get_result = result  # 다른 버튼 에서 data를 사용하기 위해 변수 설정
+
+        self.top_axes.plot(result.index, result['Close'], label=id)
         self.top_axes.legend(loc='best')
         self.top_axes.grid()
+        self.top_axes.set_ylabel('원')
         self.canvas1.draw()
+
+        self.top_axes.set_title(id)  # 종목 이름을 타이틀로 설정
 
         self.pushButton_2.setEnabled(True)
         self.pushButton_3.setEnabled(True)
         self.pushButton_4.setEnabled(True)
         self.pushButton_5.setEnabled(True)
         self.pushButton_6.setEnabled(True)
+        self.pushButton_8.setEnabled(True)
+        self.pushButton_9.setEnabled(True)
+        self.pushButton_10.setEnabled(True)
 
     def btn2_clicked(self):
         result = self.get_result
@@ -130,9 +170,9 @@ class MyWindow(QMainWindow, form_class):
             msg.showwarning('안내', '종목조회를 먼저해주세요')
             return
 
-        Close5 = result['Adj Close'].rolling(window=5).mean()
+        Close5 = result['Close'].rolling(window=5).mean()
         result.insert(len(result.columns), "Close5", Close5)
-        self.top_axes.plot(result.index, result['Close5'], '-.', label="Close5")
+        self.top_axes.plot(result.index, result['Close5'], '-.', label="5일 종가 평균")
         self.top_axes.legend(loc='best')
         self.canvas1.draw()
 
@@ -144,9 +184,9 @@ class MyWindow(QMainWindow, form_class):
             msg.showwarning('안내', '종목조회를 먼저해주세요')
             return
 
-        Close20 = result['Adj Close'].rolling(window=20).mean()
+        Close20 = result['Close'].rolling(window=20).mean()
         result.insert(len(result.columns), "Close20", Close20)
-        self.top_axes.plot(result.index, result['Close20'], '--', label="Close20")
+        self.top_axes.plot(result.index, result['Close20'], '--', label="20일 종가 평균")
         self.top_axes.legend(loc='best')
         self.canvas1.draw()
 
@@ -158,9 +198,9 @@ class MyWindow(QMainWindow, form_class):
             msg.showwarning('안내', '종목조회를 먼저해주세요')
             return
 
-        Close60 = result['Adj Close'].rolling(window=60).mean()
+        Close60 = result['Close'].rolling(window=60).mean()
         result.insert(len(result.columns), "Close60", Close60)
-        self.top_axes.plot(result.index, result['Close60'], ':', label="Close60")
+        self.top_axes.plot(result.index, result['Close60'], ':', label="60일 종가 평균")
         self.top_axes.legend(loc='best')
         self.canvas1.draw()
 
@@ -172,9 +212,9 @@ class MyWindow(QMainWindow, form_class):
             msg.showwarning('안내', '종목조회를 먼저해주세요')
             return
 
-        Close120 = result['Adj Close'].rolling(window=120).mean()
+        Close120 = result['Close'].rolling(window=120).mean()
         result.insert(len(result.columns), "Close120", Close120)
-        self.top_axes.plot(result.index, result['Close120'], label="Close120")
+        self.top_axes.plot(result.index, result['Close120'], label="120일 종가 평균")
         self.top_axes.legend(loc='best')
         self.canvas1.draw()
 
@@ -182,6 +222,7 @@ class MyWindow(QMainWindow, form_class):
 
     # barGraph
     def btn6_clicked(self):
+        self.bottom_axes.cla()
         result = self.get_result
         if result is None:
             msg.showwarning('안내', '종목조회를 먼저해주세요')
@@ -195,6 +236,71 @@ class MyWindow(QMainWindow, form_class):
         self.canvas1.draw()
 
         self.pushButton_6.setEnabled(False)
+        self.pushButton_10.setEnabled(True)
+
+    def btn7_clicked(self):
+        self.delete_canvas()
+        self.create_canvas()
+
+    def btn8_clicked(self):
+        id = 'KS11'
+
+        start = self.dateEdit.date().toString("yyyy-MM-dd")
+        end = self.dateEdit_2.date().toString("yyyy-MM-dd")
+
+        result = self.SDR.get_stock(id, start, end)
+
+        self.get_result = result  # 다른 버튼 에서 data를 사용하기 위해 변수 설정
+
+        self.top_axes.plot(result.index, result['Close'], label='코스피')
+        self.top_axes.legend(loc='best')
+        self.canvas1.draw()
+
+        self.pushButton_8.setEnabled(False)
+
+    def btn9_clicked(self):
+        id = 'IXIC'
+
+        start = self.dateEdit.date().toString("yyyy-MM-dd")
+        end = self.dateEdit_2.date().toString("yyyy-MM-dd")
+
+        result = self.SDR.get_stock(id, start, end)
+        print(result)
+        self.get_result = result  # 다른 버튼 에서 data를 사용하기 위해 변수 설정
+
+        self.top_axes.plot(result.index, result['Close'], label='나스닥')
+        self.top_axes.legend(loc='best')
+        self.canvas1.draw()
+
+        self.pushButton_9.setEnabled(False)
+
+    def btn10_clicked(self):
+        self.bottom_axes.cla()
+        result = self.get_result
+        if result is None:
+            msg.showwarning('안내', '종목조회를 먼저해주세요')
+            return
+
+        # 거래량 값으로서 큰 값이 발생할 때 그 값을 오일러 상수(e)의 지수 형태로 표현되지 않게 해줍니다.
+        self.bottom_axes.get_yaxis().get_major_formatter().set_scientific(False)
+
+        day_list = []
+        name_list = []
+        for i, day in enumerate(result.index):
+            if day.dayofweek == 0:
+                day_list.append(i)
+                name_list.append(day.strftime('%d') + '(Mon)')
+
+        self.bottom_axes.xaxis.set_major_locator(ticker.FixedLocator(day_list))
+        self.bottom_axes.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
+
+        mpl_finance.candlestick2_ohlc(self.bottom_axes, result['Open'], result['High'], result['Low'], result['Close'], width=0.5,
+                                      colorup='r', colordown='b')
+        self.bottom_axes.grid()
+        self.canvas1.draw()
+
+        self.pushButton_10.setEnabled(False)
+        self.pushButton_6.setEnabled(True)
 
     def if_integer(self, st):
         reg_exp = "[-+]?\d+$"
@@ -206,3 +312,64 @@ if __name__ == "__main__":
     myWindow = MyWindow()
     myWindow.show()
     app.exec_()
+
+
+    # pandas_datareader.data 에서 사용한 방식
+    # def btn1_clicked(self):
+    #     if self.fig1 is None:
+    #         self.create_canvas()
+    #     else:
+    #         self.delete_canvas()
+    #         self.create_canvas()
+    #     # 조회
+    #     id = self.lineEdit.text()
+    #     if self.if_integer(id):  # 종목코드 또는 숫자로 입력했을 경우
+    #         msg.showwarning('안내', '주식의 종목이름을 정확히 입력해주세요')
+    #         return
+    #
+    #     code = self.SDR.get_stock_code(id)
+    #     if code is None:
+    #         msg.showwarning('안내', '주식의 종목이름을 정확히 입력해주세요')
+    #         return
+    #
+    #     print(code)
+    #
+    #     engine = self.comboBox.currentText()
+    #     print(engine)
+    #
+    #     try:
+    #
+    #         if engine == 'yahoo':
+    #             if code in '.ks':
+    #                 result = self.SDR.get_stock(code, engine, start, end)
+    #
+    #                 self.get_result = result
+    #
+    #                 self.top_axes.plot(result.index, result['Close'], label='Close')
+    #                 self.top_axes.legend(loc='best')
+    #                 self.top_axes.grid()
+    #                 self.canvas1.draw()
+    #             else:  # 야후파이넨스에서 코스닥 최근 5년 기록을 읽어 오지 못 함
+    #                 raise RemoteDataError
+    #         else:
+    #             print(code)
+    #             result = self.SDR.get_stock(code, engine, start, end)
+    #
+    #             self.get_result = result
+    #
+    #             self.top_axes.plot(result['Close'], label='Close')
+    #             self.top_axes.legend(loc='best')
+    #             self.top_axes.grid()  # 격자 그리기
+    #             self.canvas1.draw()
+    #
+    #     except RemoteDataError:
+    #         msg.showwarning('안내',  engine+'에는 해당종목의 검색결과가 존재하지 않습니다')
+    #         return
+    #
+    #     self.top_axes.set_title(id)  # 종목 이름을 타이틀로 설정
+    #
+    #     self.pushButton_2.setEnabled(True)
+    #     self.pushButton_3.setEnabled(True)
+    #     self.pushButton_4.setEnabled(True)
+    #     self.pushButton_5.setEnabled(True)
+    #     self.pushButton_6.setEnabled(True)
--- pythonProject6/stockReader.py
+++ pythonProject6/stockReader.py
@@ -1,28 +1,38 @@
 import pandas_datareader.data as web
 import datetime
-import pandas as pd
 import numpy as np
+from pandas_datareader._utils import RemoteDataError
 from selenium import webdriver
 import requests
 import re
 from bs4 import BeautifulSoup
 import csv
+import pandas_datareader.naver as web_naver
+import FinanceDataReader as fdr
+import pandas
+import requests
+from io import BytesIO
 
-
-url = 'https://finance.naver.com/'
 
 class stockReader():
-    # def __init__(self):
-    #     super().__init__()
-    #     self.get_stock_code('3S')
 
     def get_stock_code(self, id):
-        f = open('KOSDAQ.csv', 'r')
+        # 깔끔하게 .csv 파일로 만들어주는 사이트와 라이브러리가 있는데 사용안할 이유가 없음
+        # url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={}'.format(id)
+        # res = requests.get(url=url)
+        # find_code = BeautifulSoup(res.text, "html.parser")
+        # code = find_code.select_one('em.t_nm')
+        # print(code.text)
+        # print(id)
+        # return code.text
+
+        f = open('filename.csv', 'r')
         readFile = csv.reader(f)
         code = None
         for data in readFile:
-            if id == data[1]:
-                code = data[0]
+            if id == data[3]:
+                # code = data[0] + '.kq'  # pandas_datareader.data 에서 사용한 방식
+                code = data[2]
         f.close()
 
         if code == None:
@@ -30,41 +40,56 @@ class stockReader():
             readFile = csv.reader(f)
             for data in readFile:
                 if id == data[1]:
+                    # code = data[0] + '.ks'  # pandas_datareader.data 에서 사용한 방식
                     code = data[0]
             f.close()
 
         return code
 
-    def get_stock(self, name, engine, b_day, e_day):
-
-        data = web.DataReader(name, engine, b_day, e_day)
+    def get_stock(self, name, b_day, e_day):
+        data = fdr.DataReader(name, b_day, e_day)
         nonZdata = data[data['Volume'] != 0]
-
-        print(nonZdata)
-
         return nonZdata
 
+    def write_csv(self):
+        KO = ['STK', 'KSQ']  # STK = kospi   KSQ = kosdaq
+        fileName = ['KOSPI.csv', 'KOSDAQ.csv']
+
+        for i in range(2):
+            # 0. 공통 헤더
+            headers = {'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'}
+
+            # 1. OTP 받기
+            otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+            otp_data = {
+                'locale': 'ko_KR',
+                'mktId': KO[i],
+                'share': '1',
+                'csvxls_isNo': 'false',
+                'name': 'fileDown',
+                'url': 'dbms/MDC/STAT/standard/MDCSTAT01901'
+            }
+            otp = requests.post(otp_url, otp_data, headers=headers).text
+
+            # 2. 데이터 다운로드
+            data_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
+            data = requests.post(data_url, {'code': otp}, headers=headers)
+
+            # 3. 판다스 DataFrame으로 변환
+            df = pandas.read_csv(BytesIO(data.content), encoding='EUC-KR')
+
+            # 4. 코스닥.csv 파일 저장
+            df.to_csv(fileName[i], mode='w', encoding="ANSI")
 
 
-# c = stockReader()
-# Close5 = new_gs['Adj Close'].rolling(window=5).mean()
-# new_gs.insert(len(new_gs.columns), "Close5", Close5)
-#
-# Close20 = new_gs['Adj Close'].rolling(window=20).mean()
-# new_gs.insert(len(new_gs.columns), "Close20", Close20)
-#
-# Close60 = new_gs['Adj Close'].rolling(window=60).mean()
-# new_gs.insert(len(new_gs.columns), "Close60", Close60)
-#
-# Close120 = new_gs['Adj Close'].rolling(window=120).mean()
-# new_gs.insert(len(new_gs.columns), "Close120", Close120)
-#
-# plt.plot(new_gs.index, new_gs['Adj Close'], label='Adj Close')
-# plt.plot(new_gs.index, new_gs['Close5'], label="Close5")
-# plt.plot(new_gs.index, new_gs['Close20'], label="Close20")
-# plt.plot(new_gs.index, new_gs['Close60'], label="Close60")
-# plt.plot(new_gs.index, new_gs['Close120'], label="Close120")
-#
-# plt.legend(loc='best')
-# plt.grid()
-# plt.show()
+
+    # 야후는 코스닥 기록을 가져오질 못하고 네이버는 y축 값에 오류가있어서 그래프가 망가짐
+    # def get_stock(self, name, engine, b_day, e_day):
+    #     # if engine == 'yahoo':
+    #     #     data = web.DataReader(name, engine, b_day, e_day)
+    #     #     nonZdata = data[data['Volume'] != 0]
+    #     #     return nonZdata
+    #     # else:
+    #     #     data = web.DataReader(name, engine, b_day, e_day)
+    #     #     nonZdata = data[data['Volume'] != 0]
+    #     #     return nonZdata
